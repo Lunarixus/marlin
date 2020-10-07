aosp-mirror/kernel_msm

ion: add heap size change tracepoints
This change adds tracepoints to log when the size of the ion heap
changes. This helps identify processes that are using a significant
amount of ion memory.

Change-Id: Ia43a8699eb5db95741e50d8746fb6825efc9ed14
Signed-off-by: Tim Murray <timmurray@google.com>

drivers/staging/android/ion/ion.c
@@ -265,6 +265,8 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 	mutex_lock(&dev->buffer_lock);
 	ion_buffer_add(dev, buffer);
 	mutex_unlock(&dev->buffer_lock);
+	trace_ion_heap_grow(heap->name, len,
+				atomic_long_read(&heap->total_allocated));
 	atomic_long_add(len, &heap->total_allocated);
 	return buffer;
 
@@ -286,6 +288,8 @@ void ion_buffer_destroy(struct ion_buffer *buffer)
 	}
 	buffer->heap->ops->unmap_dma(buffer->heap, buffer);
 
+	trace_ion_heap_shrink(buffer->heap->name,  buffer->size,
+				atomic_long_read(&buffer->heap->total_allocated));
 	atomic_long_sub(buffer->size, &buffer->heap->total_allocated);
 	buffer->heap->ops->free(buffer);
 	vfree(buffer->pages);

Lunarixus/marlin

Bug: 121158612
Test: manual, saw the tracepoints in a trace

drivers/staging/android/ion/ion.c
@@ -268,6 +268,8 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 	mutex_lock(&dev->buffer_lock);
 	ion_buffer_add(dev, buffer);
 	mutex_unlock(&dev->buffer_lock);
+	trace_ion_heap_grow(heap->name, len,
+				atomic_read(&heap->total_allocated));
 	atomic_add(len, &heap->total_allocated);
 	return buffer;
 
@@ -288,6 +290,8 @@ void ion_buffer_destroy(struct ion_buffer *buffer)
 		buffer->heap->ops->unmap_kernel(buffer->heap, buffer);
 	buffer->heap->ops->unmap_dma(buffer->heap, buffer);
 
+	trace_ion_heap_shrink(buffer->heap->name,  buffer->size,
+				atomic_read(&buffer->heap->total_allocated));
 	atomic_sub(buffer->size, &buffer->heap->total_allocated);
 	buffer->heap->ops->free(buffer);
 	if (buffer->pages)